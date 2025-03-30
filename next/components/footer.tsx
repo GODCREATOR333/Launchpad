24a87e6a48e7c280cbc18058e4caf96170e6aa3e
GODCREATOR333 <haripreetham.jntuh@gmail.com>
2025-03-30 10:27:45 +0530

    tailwindcss update removed, other dependencies adjusted

--- next/components/footer.tsx
+++ next/components/footer.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Logo } from "@/components/logo";
 import { Link } from "next-view-transitions";
 
-
 export const Footer = async ({ data, locale }: { data: any, locale: string }) => {
-  
   return (
     <div className="relative">
       <div className="border-t border-neutral-900 px-8 pt-20 pb-32 relative bg-primary">
